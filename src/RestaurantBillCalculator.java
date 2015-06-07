bc3b8330a45d65da95e38066542849a32d44776b
toto <toto@wtoto7>
2015-06-07 21:58:52 +0100

    Part 1 all GUI components showing up on the GUI - this is a good point to go back if you need to have a working GUI but a lot is greyed out of the normal code

--- src/RestaurantBillCalculator.java
+++ src/RestaurantBillCalculator.java
@@ -96,8 +96,8 @@ public class RestaurantBillCalculator extends JFrame
    
    
    // constructor
-   public RestaurantBillCalculator( 
-   String databaseUserName, String databasePassword )
+   public RestaurantBillCalculator() 
+//   String databaseUserName, String databasePassword )
    {
 
       createUserInterface(); // set up GUI  
@@ -116,55 +116,105 @@ public class RestaurantBillCalculator extends JFrame
       contentPane.setLayout( null );
 
       // **** TODO ****** set up restaurantJLabel
-
+     // set up restaurantJLabel
+     restaurantJLabel = new JLabel();
+     restaurantJLabel.setBounds( 80, 8, 128, 24 );
+     restaurantJLabel.setText( "Restaurant" );
+     restaurantJLabel.setFont(
+     new Font( "SansSerif", Font.BOLD, 16 ) );
+     contentPane.add( restaurantJLabel );
 
       // **** TODO ****** set up waiterJPanel
+     // set up waiterJPanel
+     createWaiterJPanel();
+     contentPane.add( waiterJPanel );     
 
       // **** TODO ****** set up menuItemsJPanel
+     // set up menuItemsJPanel
+     createMenuItemsJPanel();
+     contentPane.add( menuItemsJPanel );
+//
+//            public void actionPerformed( ActionEvent event ) 
+//            {
+//               calculateBillJButtonActionPerformed( event );
+//            }
+//
+//         } // end anonymous inner class
 
-            public void actionPerformed( ActionEvent event ) 
-            {
-               calculateBillJButtonActionPerformed( event );
-            }
-
-         } // end anonymous inner class
-
-      ); // end addActionListener
+//      ); // end addActionListener
       
       // **** TODO ****** set up subtotalJLabel
+     // set up subtotalJLabel
+     subtotalJLabel = new JLabel();
+     subtotalJLabel.setBounds( 15, 340, 56, 16 );
+     subtotalJLabel.setText( "Subtotal:" );
+     contentPane.add( subtotalJLabel );
 
-
+     
       // **** TODO ****** set up subtotalJTextField
-      subtotalJTextField = new JTextField();
+     subtotalJTextField = new JTextField();
+     subtotalJTextField.setBounds( 70, 340, 80, 20 );
+     subtotalJTextField.setEditable( false );
+     subtotalJTextField.setBorder(
+     BorderFactory.createLoweredBevelBorder() );
+     subtotalJTextField.setHorizontalAlignment( JTextField.RIGHT );
+     contentPane.add( subtotalJTextField );
 
 
       // **** TODO ****** set up taxJLabel
-
+     // set up taxJLabel
+     taxJLabel = new JLabel();
+     taxJLabel.setBounds( 15, 375, 56, 16 );
+     taxJLabel.setText( "Tax:" );
+     contentPane.add( taxJLabel );
 
       // **** TODO ****** set up taxJTextField
-
+     taxJTextField = new JTextField();
+     taxJTextField.setBounds( 70, 375, 80, 20 );
+     taxJTextField.setEditable( false );
+     taxJTextField.setBorder(
+     BorderFactory.createLoweredBevelBorder() );
+     taxJTextField.setHorizontalAlignment( JTextField.RIGHT );
+     contentPane.add( taxJTextField );
 
       // **** TODO ****** set up totalJLabel
-
+     totalJLabel = new JLabel();
+     totalJLabel.setBounds( 15, 410, 56, 16 );
+     totalJLabel.setText( "Total:" );
+     contentPane.add( totalJLabel );
+     
       // **** TODO ****** set up totalJTextField
-
+     totalJTextField = new JTextField();
+     totalJTextField.setBounds( 70, 410, 80, 20 );
+     totalJTextField.setEditable( false );
+     totalJTextField.setBorder(
+     BorderFactory.createLoweredBevelBorder() );
+     totalJTextField.setHorizontalAlignment( JTextField.RIGHT );
+     contentPane.add( totalJTextField );
+     
+     
       // **** TODO ****** set properties of application's window
+     setTitle( "Restaurant Bill Calculator" ); // set window title 
+     setSize( 280, 500 ); // set window size
+     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//stop program when close the jframe
+     setVisible( true ); // display window
+
 
       // **** TODO ****** ensure database connection is closed
-      // **** TODO ****** when user quits application
-      addWindowListener(
-
-         new WindowAdapter()anonymous inner class
-         {
-            //  event handler called when close button is clicked
-            public void windowClosing( WindowEvent event )
-            {
-               frameWindowClosing( event );
-            }
-
-         } //  end anonymous inner class
-
-      ); // end addWindowListener
+//      // **** TODO ****** when user quits application
+//      addWindowListener(
+//
+//         new WindowAdapter()anonymous inner class
+//         {
+//            //  event handler called when close button is clicked
+//            public void windowClosing( WindowEvent event )
+//            {
+//               frameWindowClosing( event );
+//            }
+//
+//         } //  end anonymous inner class
+//
+//      ); // end addWindowListener
 
    } // end method createUserInterface
 
@@ -172,15 +222,44 @@ public class RestaurantBillCalculator extends JFrame
    private void createWaiterJPanel()
    {
       // **** TODO ****** set up waiterJPanel
-
+     waiterJPanel = new JPanel();
+     waiterJPanel.setBounds( 20, 48, 232, 88 );
+     waiterJPanel.setBorder( BorderFactory.createTitledBorder(
+     BorderFactory.createEtchedBorder(),
+     "Waiter Information" ) );
+     waiterJPanel.setLayout( null );
 
       // **** TODO ****** set up tableNumberJLabel
+      //Create tableNumberJLabel in waiterJPanel
+      //waiterJPanel.setLayout(null);//NO layout manager in use
+      tableNumberJLabel = new JLabel();//Create tableNumberJLabel in waiterJPanel
+      tableNumberJLabel.setText("Table number:");//JLabel table number in waiterJPanel
+      tableNumberJLabel.setBounds(30, 25, 90, 20);// Size and position of tableNumberJLabel in Waiter information
+      //tableNumberJLabel.setBorder(BorderFactory.createLineBorder(Color.black));
+      waiterJPanel.add(tableNumberJLabel);//Add tableNumberJLabel onto waiterJPane     
 
       // **** TODO ****** set up tableNumberJTextField
-
+      //Create tableNumberJTextField in waiterJPanel
+      tableNumberJTextField = new JTextField();//Create tableNumberJTextField in waiterJPanel
+      tableNumberJTextField.setHorizontalAlignment(SwingConstants.RIGHT);//
+      tableNumberJTextField.setBounds(125, 25, 90, 20);// Size and position of tableNumberJTextField in Waiter information
+      waiterJPanel.add(tableNumberJTextField);//Add tableNumberJTextField onto waiterJPanel
+       
       // **** TODO ****** set up waiterNameJLabel
+      //Create waiterNameJLabel in waiterJPanel
+      waiterJPanel.setLayout(null);//NO layout manager in use
+      waiterNameJLabel = new JLabel();//Create waiterNameJLabel in waiterJPanel
+      waiterNameJLabel.setText("Waiter name:");//JLabel Waiter name in waiterJPanel
+      waiterNameJLabel.setBounds(30, 50, 90, 20);// Size and position of waiterNameJLabel in Waiter information
+      //waiterNameJLabel.setBorder(BorderFactory.createLineBorder(Color.black));
+      waiterJPanel.add(waiterNameJLabel);//Add waiterNameJLabel onto waiterJPanel      
 
       // **** TODO ****** set up waiterNameJTextField
+      //Create tableNumberJTextField in waiterJPanel
+      tableNumberJTextField = new JTextField();//Create tableNumberJTextField in waiterJPanel
+      tableNumberJTextField.setHorizontalAlignment(SwingConstants.RIGHT);//
+      tableNumberJTextField.setBounds(125, 50, 90, 20);// Size and position of tableNumberJTextField in Waiter information
+      waiterJPanel.add(tableNumberJTextField);//Add tableNumberJTextField onto waiterJPanel
 
    } // end method createWaiterJPanel
 
@@ -191,7 +270,8 @@ public class RestaurantBillCalculator extends JFrame
       menuItemsJPanel = new JPanel();
       menuItemsJPanel.setBounds( 20, 152, 232, 152 );
       menuItemsJPanel.setBorder( BorderFactory.createTitledBorder( 
-         BorderFactory.createEtchedBorder(), "Menu Items" ) );
+      BorderFactory.createEtchedBorder(), "Menu Items" ) );
+//      menuItemsJPanel.setEnabled( false ); - delete this
       menuItemsJPanel.setLayout( null );
 
       //  set up beverageJLabel
@@ -202,52 +282,102 @@ public class RestaurantBillCalculator extends JFrame
 
       // set up beverageJComboBox
       beverageJComboBox = new JComboBox();
-      beverageJComboBox.setBounds( 88, 24, 128, 25 );
+      beverageJComboBox.setBounds( 88, 24, 128, 24 );
+      beverageJComboBox.setEnabled( false );
       menuItemsJPanel.add( beverageJComboBox );
-      beverageJComboBox.addItemListener(
+//      beverageJComboBox.addItemListener(
 
-         new ItemListener()  // **** TODO ****** anonymous inner class
-         {
-            // **** TODO ****** event handler called when item in beverageJComboBox
-            // **** TODO ****** is selected
-            public void itemStateChanged( ItemEvent event )
-            {
-               beverageJComboBoxItemStateChanged( event );
-            }
+//         new ItemListener()  // **** TODO ****** anonymous inner class
+//         {
+//            // **** TODO ****** event handler called when item in beverageJComboBox
+//            // **** TODO ****** is selected
+//            public void itemStateChanged( ItemEvent event )
+//            {
+//               beverageJComboBoxItemStateChanged( event );
+//            }
 
-         } // end anonymous inner class
-
-      ); // end addItemListener
-
-      // **** TODO ****** add items to beverageJComboBox
-
+//         } // end anonymous inner class
+//
+//      ); // end addItemListener
+//
+//      // **** TODO ****** add items to beverageJComboBox
+//      // add items to beverageJComboBox
+//      beverageJComboBox.addItem( "" );
+//      loadCategory( "Beverage", beverageJComboBox );
+//        }
+//      } // end anonymous inner class
+//     ); // end addItemListener
 
       // **** TODO ****** set up appetizerJLabel
-
+      appetizerJLabel = new JLabel();
+      appetizerJLabel.setBounds( 8, 55, 80, 24 );
+      appetizerJLabel.setText( "Appetizer:" );
+      menuItemsJPanel.add( appetizerJLabel );
 
       // **** TODO ****** set up appetizerJComboBox
-
+      appetizerJComboBox = new JComboBox();
+      appetizerJComboBox.setBounds( 88, 55, 128, 24 );
+      appetizerJComboBox.setEnabled( false );
+      menuItemsJPanel.add( appetizerJComboBox );
+//    appetizerJComboBox.addItemListener(
       // **** TODO ****** add items to appetizerJComboBox
 
-
       // **** TODO ****** set up mainCourseJLabel
-
+      mainCourseJLabel = new JLabel();
+      mainCourseJLabel.setBounds( 8, 86, 80, 24 );
+      mainCourseJLabel.setText( "Main Course:" );
+      menuItemsJPanel.add( mainCourseJLabel );
+      
       // **** TODO ****** set up mainCourseJComboBox
       mainCourseJComboBox = new JComboBox();
-
+      mainCourseJComboBox.setBounds( 88, 86, 128, 24 );
+      mainCourseJComboBox.setEnabled( false );
+      menuItemsJPanel.add( mainCourseJComboBox );
+//      menuItemsJPanel.addItemListener(
       // **** TODO ****** add items to mainCourseJComboBox
 
 
       // **** TODO ****** set up dessertJLabel
-
-
+      dessertJLabel = new JLabel();
+      dessertJLabel.setBounds( 8, 117, 80, 24 );
+      dessertJLabel.setText( "Dessert:" );
+      menuItemsJPanel.add( dessertJLabel );
+      
+     // **** TODO ****** set up dessertJComboBox 
+      dessertJComboBox = new JComboBox();
+      dessertJComboBox.setBounds( 88, 117, 128, 24 );
+      dessertJComboBox.setEnabled( false );
+      menuItemsJPanel.add( dessertJComboBox );
+//      dessertJComboBox.addItemListener(
+      // **** TODO ****** add items to dessertJComboBox
+      
+      
    } // end method createMenuItemsJPanel
 
+   
    // **** TODO ****** add items to JComboBox
    private void loadCategory(
       String category, JComboBox categoryJComboBox )
    {
-            
+     // read all items from database for specified category
+    try
+    {
+    // obtain all items in specified category
+    myResultSet = myStatement.executeQuery( "SELECT name FROM "
+    + "menu WHERE category = '" + category + "'" );
+    // add items to JComboBox
+    while ( myResultSet.next() == true )
+    {
+    categoryJComboBox.addItem(
+    myResultSet.getString( "name" ) );
+    }
+    myResultSet.close(); // close myResultSet
+    } // end try
+    // catch SQLException
+    catch ( SQLException exception )
+    {
+    exception.printStackTrace();
+    }       
    } // end method loadCategory
 
    // **** TODO ****** user select beverage
@@ -280,18 +410,18 @@ public class RestaurantBillCalculator extends JFrame
       ActionEvent event )
    {
       
-   } // end method calculateBillJButtonActionPerformed
-
-   // **** TODO ****** calculate subtotal
-   private double calculateSubtotal()
-   {
-      return 0;
-
-   } // end method calculateSubtotal
-
-   // **** TODO ****** user close window
-   private void frameWindowClosing( WindowEvent event )
-   {
+//   } // end method calculateBillJButtonActionPerformed
+//
+//   // **** TODO ****** calculate subtotal
+//   private double calculateSubtotal()
+//   {
+//      return 0;
+//
+//   } // end method calculateSubtotal
+//
+//   // **** TODO ****** user close window
+//   private void frameWindowClosing( WindowEvent event )
+//   {
       
    }  // end method frameWindowClosing
 
@@ -299,22 +429,22 @@ public class RestaurantBillCalculator extends JFrame
    public static void main( String[] args ) 
    {
       // **** TODO ****** check command-line arguments
-      if ( args.length == 2 )
-      {
+//      if ( args.length == 2 )
+//      {
          // **** TODO ****** get command-line arguments
-         String databaseUserName = args[ 0 ];
-         String databasePassword = args[ 1 ];
+//         String databaseUserName = args[ 0 ];
+//         String databasePassword = args[ 1 ];
 
          // **** TODO ****** create new RestaurantBillCalculator
          RestaurantBillCalculator application = 
             new RestaurantBillCalculator ( );
 //               databaseUserName, databasePassword );
-      }
-      else
-      {
-         System.out.println( "Usage: java " + 
-            "RestaurantBillCalculator databaseUserName databasePassword" );
-      }
+//      }
+//      else
+//      {
+//         System.out.println( "Usage: java " + 
+//            "RestaurantBillCalculator databaseUserName databasePassword" );
+//    }
 
    } // end method main
 
